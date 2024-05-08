8593d94b830147d376ffcae3a3129ff6dba19941
GithunStuf <khouas.amine.pro@gmail.com>
2024-05-08 16:08:37 +0100

    commit

--- Models/Users.js
+++ Models/Users.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
     },
     lastName: {
         type: String,
-        required: true,
     },
     username: {
         type: String,
