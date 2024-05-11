bf3e721bb0e3a8c5145bc7bc0759eede892b9025
GithunStuf <khouas.amine.pro@gmail.com>
2024-05-11 13:16:36 +0100

    verify

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Controller = require('../Controllers/Auth_Controller');
-const { verifyTokenAndAuthorization } = require('../Controllers/verifyToken');
+const { verifyToken } = require('../Controllers/verifyToken');
 
 router.post('/register', Controller.addUser);
 router.post('/login', Controller.authUser);
