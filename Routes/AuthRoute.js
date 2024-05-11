1d8a705716233aefb05066d0f1344a96e61a4fd5
GithunStuf <khouas.amine.pro@gmail.com>
2024-05-11 11:45:54 +0100

    m

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const Controller = require('../Controllers/Auth_Controller')
+const Controller = require('../Controllers/Auth_Controller');
+const { verifyTokenAndAuthorization } = require('../Controllers/verifyToken');
 
 router.post('/register', Controller.addUser);
 router.post('/login', Controller.authUser);
+router.post('/',verifyTokenAndAuthorization, Controller.userData);
 
 
 module.exports = router;
